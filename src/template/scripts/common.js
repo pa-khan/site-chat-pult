b2a1d956ed7d436019cbc859fcc92193bb53e30a
e5ash <io@e5ash.com>
2018-04-20 22:22:15 +0300

    100%

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -1,5 +1,18 @@
 $(document).ready(function($) {
 
-	$('.text_phone').mask('+7 (999) 999-99-99');
+	$('.input_tel input').mask('+7 (000) 000-00-00');
 	
+	$('.advantages__wrap_slider').slick()
+
+	$('.mobile-btn').click(function() {
+		$(this).toggleClass('mobile-btn_toggle');
+		$('.nav').toggleClass('nav_toggle');
+		$('.bg-mobile').toggleClass('bg-mobile_toggle');
+	});
+
+	$('.popup__show span').click(function() {
+		$('.popup__right_hidden-mobile').slideToggle(300);
+		$('.popup__desc').hide();
+		$(this).hide();
+	});
 });
